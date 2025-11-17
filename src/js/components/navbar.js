6ccc5288d68a7ccb522fde253e569ec7dd457687
tenseoverflow <contact@hen.ee>
2025-11-17 11:26:42 +0200

    fix: settings and navbar parameter

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -8,12 +8,14 @@ class AppNavbar extends HTMLElement {
     if (this._initialized) return;
     this._initialized = true;
 
+    const title = this.getAttribute("title") || "Ruumibroneerimine";
+
     this.innerHTML = `
       <nav class="navbar">
         <div class="navitems">
           <div class="nav-left">
             <a class="nav-link" href="/booking/">Kodu</a>
-            <p>Ruumibroneerimine</p>
+            <p>${title}</p>
           </div>
           <div class="nav-right">
             <div class="button-navbar">
